6e69798692f6c3daf5d25489d815492d61f25aa1
EvrenTank <evrentank@gmail.com>
2023-10-08 15:04:02 +0300

    changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors());
+const PORT = process.env.PORT || 8080;
 
 app.get('/',(req,res)=>{
     res.json('deneme');
@@ -13,10 +14,10 @@ app.get('/manga/:mangaName/:episodeNumber/:pageNumber',(req,res)=>{
     const episodeNumber = req.params.episodeNumber;
     const pageNumber = req.params.pageNumber;
     const imagePath = `${__dirname}/manga/${mangaName}/${episodeNumber}/${pageNumber}.jpg`;
-  
+    console.log('imagePath');
     res.sendFile(imagePath);
 });
 
-app.listen(8080,()=>{
-    console.log('Server is running on port 8080')
+app.listen(PORT,()=>{
+    console.log('Server is running on port',PORT)
 })
