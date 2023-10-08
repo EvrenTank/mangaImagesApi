aa0e3159fcb5785c4994e318ef3d9bd4d7028c70
EvrenTank <evrentank@gmail.com>
2023-10-08 12:32:34 +0300

    fixed I guess

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const uploadPath = './manga';
+app.use(cors());
 
-app.get('manga/:mangaName/:episodeNumber/:pageNumber',(req,res)=>{
+app.get('/',(req,res)=>{
+    res.json('deneme');
+})
+
+app.get('/manga/:mangaName/:episodeNumber/:pageNumber',(req,res)=>{
     const mangaName = req.params.mangaName;
     const episodeNumber = req.params.episodeNumber;
     const pageNumber = req.params.pageNumber;
-    const imagePath = `manga/${mangaName}/${episodeNumber}/${pageNumber}`;
-
+    const imagePath = `${__dirname}/manga/${mangaName}/${episodeNumber}/${pageNumber}.jpg`;
+  
     res.sendFile(imagePath);
 });
 
 app.listen(8080,()=>{
-    console.log('Server is')
+    console.log('Server is running on port 8080')
 })
